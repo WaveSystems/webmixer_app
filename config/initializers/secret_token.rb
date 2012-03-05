06de03fd96f2c127aa95a2cb8852dc254d9d7e8a
Antonio Chavez <cavjzz@gmail.com>
2012-03-05 10:24:19 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebmixerApp::Application.config.secret_token = 'ce304b67b75812f722e904c283eef852aeb901bf0c3333316cd8990ecf698064eac8450b901097a085ea6b131f94b986d5331faaaf5579a61258bae4654607a3'
