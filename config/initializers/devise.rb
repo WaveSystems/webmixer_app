b0bcfe20f063c43bf961689561432e705f92317b
Antonio Chavez <cavjzz@gmail.com>
2012-03-13 10:32:53 -0600

    Changed route to app

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,10 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "please-change-me-at-config-initializers-devise@example.com"
+
+  config.mailer_sender = "cavjzz@hotmail.com"
+  config.rpx_application_name = 'webmixer'
+  RPXNow.api_key = "166484976790000" # real key goes here.
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
