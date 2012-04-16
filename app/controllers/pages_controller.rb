51ba6846e7fb37af6517d17b07b3177d3f407546
Antonio Chavez <cavjzz@gmail.com>
2012-04-15 21:44:12 -0500

    Refactored index view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
+  before_filter :authenticate_user!, :except => :index
   def index
+    if user_signed_in?
+      redirect_to '/decks'
+    end
+  end
+  
+  def decks
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ WebmixerApp::Application.routes.draw do
   devise_for :users
 
   root :to => 'pages#index'
+  get '/decks' => 'pages#decks'
 end
